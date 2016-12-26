1292e75bb11912a9e45135c842b3c7c98fdd2be8
Brandon Pitman <brandon.pitman@gmail.com>
2016-12-26 02:31:42 +0000

    Update TLS configuration settings.
    The settings used are the "modern" configuration from "Exposing Go on the Internet" (https://blog.gopheracademy.com/advent-2016/exposing-go-on-the-internet/).

--- www.go
+++ www.go
@@ -93,14 +93,20 @@ func main() {
 
 	// Start serving.
 	config := &tls.Config{
-		// TODO(bran): why does this not work with LetsEncrypt?
-		//MinVersion: tls.VersionTLS10,
-		//CipherSuites: []uint16{
-		//	tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-		//	tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-		//	tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
-		//	tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA,
-		//},
+		PreferServerCipherSuites: true,
+		CurvePreferences: []tls.CurveID{
+			tls.CurveP256,
+			tls.X25519,
+		},
+		MinVersion: tls.VersionTLS12,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+		},
 		GetCertificate: m.GetCertificate,
 	}
 	server := &http.Server{
