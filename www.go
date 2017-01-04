64eafafb53b90b49360e3c76dda9ee160b810994
Brandon Pitman <brandon.pitman@gmail.com>
2017-01-03 20:14:32 -0800

    Clean up code.
     * Remove global state.
     * Clean up unnecessary pointer chasing.
     * Move HTTP handling into a single function.

--- www.go
+++ www.go
@@ -20,58 +20,62 @@ const (
 	certDir = "/var/lib/www/certs"
 )
 
-var (
-	mux *http.ServeMux
-)
-
 type loggingHandler struct {
 	h       http.Handler
 	logName string
 }
 
-func (lh *loggingHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (lh loggingHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	log.Printf("[%s] %s requested %s", lh.logName, r.RemoteAddr, r.RequestURI)
 	lh.h.ServeHTTP(w, r)
 }
 
-func NewLoggingHandler(logName string, h http.Handler) *loggingHandler {
-	return &loggingHandler{
+func NewLoggingHandler(logName string, h http.Handler) http.Handler {
+	return loggingHandler{
 		h:       h,
 		logName: logName,
 	}
 }
 
-func contentHandler(w http.ResponseWriter, r *http.Request) {
+type secureHeaderHandler struct {
+	h http.Handler
+}
+
+func (shh secureHeaderHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload")
 	w.Header().Add("Content-Security-Policy", "default-src 'self'; style-src 'self' https://fonts.googleapis.com; font-src https://fonts.gstatic.com")
 	w.Header().Add("X-Frame-Options", "SAMEORIGIN")
 	w.Header().Add("X-XSS-Protection", "1; mode=block")
 	w.Header().Add("X-Content-Type-Options", "nosniff")
 
-	mux.ServeHTTP(w, r)
+	shh.h.ServeHTTP(w, r)
 }
 
-func redirectHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Connection", "close")
-	url := *r.URL
-	url.Scheme = "https"
-	url.Host = host
-	http.Redirect(w, r, url.String(), http.StatusMovedPermanently)
+func NewSecureHeaderHandler(h http.Handler) http.Handler {
+	return secureHeaderHandler{
+		h: h,
+	}
 }
 
-func httpHandler() {
+func serveHTTPRedirects() {
 	server := &http.Server{
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
 		IdleTimeout:  120 * time.Second,
 		Addr:         ":http",
-		Handler:      NewLoggingHandler("http ", http.HandlerFunc(redirectHandler)),
+		Handler: NewLoggingHandler("http ", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Add("Connection", "close")
+			url := *r.URL
+			url.Scheme = "https"
+			url.Host = host
+			http.Redirect(w, r, url.String(), http.StatusMovedPermanently)
+		})),
 	}
 	log.Fatalf("ListenAndServe: %v", server.ListenAndServe())
 }
 
 func main() {
-	go httpHandler()
+	go serveHTTPRedirects()
 
 	// Set up certificate handling.
 	m := autocert.Manager{
@@ -82,7 +86,7 @@ func main() {
 	}
 
 	// Set up serving mux.
-	mux = http.NewServeMux()
+	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(&assetfs.AssetFS{Asset: data.Asset, AssetDir: data.AssetDir, AssetInfo: data.AssetInfo}))
 	mux.HandleFunc("/ip", func(w http.ResponseWriter, r *http.Request) {
 		idx := strings.Index(r.RemoteAddr, ":")
@@ -119,7 +123,7 @@ func main() {
 		WriteTimeout: 10 * time.Second,
 		IdleTimeout:  120 * time.Second,
 		Addr:         ":https",
-		Handler:      NewLoggingHandler("https", http.HandlerFunc(contentHandler)),
+		Handler:      NewLoggingHandler("https", NewSecureHeaderHandler(mux)),
 		TLSConfig:    config,
 	}
 	log.Printf("Serving")
