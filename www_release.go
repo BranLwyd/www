334173e24867db3434652ae8d83392fe813c4661
Brandon Pitman <brandon.pitman@gmail.com>
2019-02-26 19:41:18 -0800

    Require TLS 1.3.

--- www_release.go
+++ www_release.go
@@ -4,6 +4,7 @@ import (
 	"crypto/tls"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"golang.org/x/crypto/acme"
@@ -15,6 +16,10 @@ const (
 	email   = "brandon.pitman@gmail.com"
 	certDir = "/home/www/certs"
 )
+
+func init() {
+	os.Setenv("GODEBUG", os.Getenv("GODEBUG")+",tls13=1") // enable TLS 1.3; remove once enabled by default
+}
 
 func serve(h http.Handler) {
 	m := autocert.Manager{
@@ -30,18 +35,10 @@ func serve(h http.Handler) {
 				tls.X25519,
 				tls.CurveP256,
 			},
-			MinVersion:             tls.VersionTLS12,
+			MinVersion:             tls.VersionTLS13,
 			SessionTicketsDisabled: true,
-			CipherSuites: []uint16{
-				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-			},
-			GetCertificate: m.GetCertificate,
-			NextProtos:     []string{"h2", acme.ALPNProto},
+			GetCertificate:         m.GetCertificate,
+			NextProtos:             []string{"h2", acme.ALPNProto},
 		},
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
